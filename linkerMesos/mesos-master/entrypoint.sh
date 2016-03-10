0efcead585685eebcc7e4ba2e5ad68069ed46255
chenyongyu <396279682@qq.om>
2016-03-10 18:11:52 +0800

    image and compose file update

--- linkerMesos/mesos-master/entrypoint.sh
+++ linkerMesos/mesos-master/entrypoint.sh
@@ -5,7 +5,4 @@ fi
 localip=`ip addr show $ENNAME|grep "inet.*brd.*$ENNAME"|awk '{print $2}'|awk -F/ '{print $1}'`
 
 export MESOS_IP=$localip
-tmpHost=${HOSTNAME//./_}
-finalHost=${tmpHost//-/_}
-export MESOS_HOSTNAME=`eval echo '$'$finalHost`
 mesos-master
--- linkerMesos/mesos-slave/entrypoint.sh
+++ linkerMesos/mesos-slave/entrypoint.sh
@@ -5,9 +5,6 @@ fi
 localip=`ip addr show $ENNAME|grep "inet.*brd.*$ENNAME"|awk '{print $2}'|awk -F/ '{print $1}'`
 
 export MESOS_IP=$localip
-tmpHost=${HOSTNAME//./_}
-finalHost=${tmpHost//-/_}
-export MESOS_HOSTNAME=`eval echo '$'$finalHost`
 
 #!set mesos attributes env
 attrLabel=`docker info | grep \s*"LINKER_MESOS_ATTRIBUTE="`
