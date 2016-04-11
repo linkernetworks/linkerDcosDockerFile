9d0df78695f7a9891b8c0eebfe8d31e15a7824ac
Ye Qing <qye@linkernetworks.com>
2016-04-11 14:19:08 +0800

    client dockerfile

---
+++ client/entrypoint.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# filename: entrypoint.sh
+
+# MONGODB_NODES examples
+# MONGODB_NODES=172.10.17.101,172.10.17.102,172.10.17.103,172.10.17.104
+# export MONGODB_NODES=172.10.17.101,172.10.17.102,172.10.17.103,172.10.17.104
+
+newline="mongod.product.uri=mongodb:\/\/"
+
+string=$MONGODB_NODES
+# split to IP array
+array=(${string//,/ })
+for i in "${!array[@]}"
+do
+    echo "${array[i]}"
+    item="${array[i]}:27017,"
+    echo "$item"
+    newline=$newline$item
+    echo $newline
+done
+
+# replace last ,
+newline=${newline::-1}
+
+echo "Final newline"
+echo $newline
+
+# replace line
+# oldline
+# mongod.product.uri=...
+# newline
+# $newline
+
+sed -i "s/mongod\.product\.uri=.*/${newline}/" /usr/local/bin/dcos_client.properties
+
+tail /usr/local/bin/dcos_client.properties
+
+# start
+# DO NOT USE nohup
+/usr/local/bin/dcos_client -config=/usr/local/bin/dcos_client.properties
+
