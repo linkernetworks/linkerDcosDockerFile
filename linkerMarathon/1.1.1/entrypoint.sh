9fc0192bdfbd1a4a2c36031f1657815fe309ea52
jiezhang <zhangjie0220@gmail.com>
2016-05-03 06:15:22 +0000

    add mesos 0.28.1, marathon 1.1.1, add zip in mesos.

---
+++ linkerMarathon/1.1.1/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ -z "$ENNAME" ];then
+    ENNAME=eth0
+fi
+localip=`ip addr show $ENNAME|grep "inet.*brd.*$ENNAME"|awk '{print $2}'|awk -F/ '{print $1}'`
+export MARATHON_HTTP_ADDRESS=$localip
+export MARATHON_HTTPS_ADDRESS=$localip
+
+hostNamePrefix="linker_hostname_"
+tmpHost=${HOSTNAME//./_}
+finalHost=${tmpHost//-/_}
+finalHost=$hostNamePrefix$finalHost 
+#export MARATHON_HOSTNAME=`eval echo '$'$finalHost`
+export MARATHON_HOSTNAME=$localip
+
+marathon
---
+++ linkerMesos/0.28.1/mesos-slave/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+if [ -z "$ENNAME" ];then
+    ENNAME=eth0
+fi
+localip=`ip addr show $ENNAME|grep "inet.*brd.*$ENNAME"|awk '{print $2}'|awk -F/ '{print $1}'`
+
+export MESOS_IP=$localip
+
+hostNamePrefix="linker_hostname_"
+tmpHost=${HOSTNAME//./_}
+finalHost=${tmpHost//-/_}
+finalHost=$hostNamePrefix$finalHost 
+advertiseip=`eval echo '$'$finalHost`
+export MESOS_HOSTNAME=$localip
+if [[ -n $advertiseip ]]; then
+    export MESOS_ADVERTISE_IP=$advertiseip
+fi
+
+#!set mesos attributes env
+attrLabel=`docker info | grep \s*"LINKER_MESOS_ATTRIBUTE="`
+if [[ -n $attrLabel ]]; then
+   value=`echo $attrLabel |awk -F '=' '{print $2}'`
+   if [[ -n $value ]]; then
+     export MESOS_ATTRIBUTES=$value
+   fi
+fi
+ 
+mesos-slave
