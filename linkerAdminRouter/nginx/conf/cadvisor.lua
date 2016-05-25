f4942dc94dc9d1f74dc6f1e0be10a5aef0d50e33
jiezhang <zhangjie0220@gmail.com>
2016-05-25 18:56:35 +0800

    add reverse proxy config to cadvisor on slave node.

---
+++ linkerAdminRouter/nginx/conf/cadvisor.lua
@@ -0,0 +1,23 @@
+local common = require "common"
+
+local state = common.mesos_get_state()
+if state == nil then
+    ngx.status = ngx.HTTP_SERVICE_UNAVAILABLE
+    ngx.say("503 Service Unavailable: invalid Mesos state.")
+    return ngx.exit(ngx.HTTP_SERVICE_UNAVAILABLE)
+end
+for _, slave in ipairs(state["slaves"]) do
+    if slave["id"] == ngx.var.slaveid then
+        local split_pid = slave["pid"]:split("@")
+        local split_sip = split_pid［2］:split(":")
+        ngx.var.slaveaddr = split_sip[1] .. ":10000"
+        ngx.log(
+            ngx.DEBUG, "cadvisor slaveid / slaveaddr:" .. 
+            ngx.var.slaveid .. " / " .. ngx.var.slaveaddr
+            )
+        return
+    end
+end
+ngx.status = ngx.HTTP_NOT_FOUND
+ngx.say("404 Not Found: slave " .. ngx.var.slaveid .. " unknown.")
+return ngx.exit(ngx.HTTP_NOT_FOUND) 
