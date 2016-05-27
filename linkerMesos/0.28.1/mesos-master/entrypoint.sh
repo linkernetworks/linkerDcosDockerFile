d047c0cbeb454dd536b48a9badd0773e601189f6
jiezhang <zhangjie0220@gmail.com>
2016-05-27 09:42:08 +0000

    install default docker version in slave image

--- linkerMesos/0.28.1/mesos-master/entrypoint.sh
+++ linkerMesos/0.28.1/mesos-master/entrypoint.sh
@@ -11,7 +11,7 @@ tmpHost=${HOSTNAME//./_}
 finalHost=${tmpHost//-/_}
 finalHost=$hostNamePrefix$finalHost 
 advertiseip=`eval echo '$'$finalHost`
-export MESOS_HOSTNAME=$localip
+export MESOS_HOSTNAME=$hostname
 if [[ -n $advertiseip ]]; then
     export MESOS_ADVERTISE_IP=$advertiseip
 fi
