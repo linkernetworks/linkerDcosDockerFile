24c593e16bb21a088a12c39b446b0e5cf76fd0c4
liu,weifeng <wliu@linkernetworks.com>
2016-07-22 15:49:32 +0800

    fix resource port error

--- linkerMesos/0.28.1/mesos-slave/entrypoint.sh
+++ linkerMesos/0.28.1/mesos-slave/entrypoint.sh
@@ -5,16 +5,7 @@ fi
 localip=`ip addr show $ENNAME|grep "inet.*brd.*$ENNAME"|awk '{print $2}'|awk -F/ '{print $1}'`
 
 export MESOS_IP=$localip
-
-# hostNamePrefix="linker_hostname_"
-# tmpHost=${HOSTNAME//./_}
-# finalHost=${tmpHost//-/_}
-# finalHost=$hostNamePrefix$finalHost 
-# advertiseip=`eval echo '$'$finalHost`
 export MESOS_HOSTNAME=$localip
-# if [[ -n $advertiseip ]]; then
-#     export MESOS_ADVERTISE_IP=$advertiseip
-# fi
 
 #!set mesos attributes env
 attrLabel=`docker info | grep \s*"LINKER_MESOS_ATTRIBUTE="`
@@ -23,12 +14,15 @@ if [[ -n $attrLabel ]]; then
    if [[ -n $value ]]; then
      export MESOS_ATTRIBUTES=$value
    fi
+   #it's shared node
    if [[ $value =~ "lb:enable" ]]; then
    	 export MESOS_DEFAULT_ROLE="slave_public"
    	 export MESOS_RESOURCES="ports(slave_public):[1-21,23-5050,5052-9999,10001-32000]"
-   else 
+   else  #pure slave node with customized label
    	 export MESOS_RESOURCES="ports(*):[1025-2180,2182-3887,3889-5049,5052-8079,8082-8180,8182-9999, 10001-32000]"
    fi
+else  # pure salve node without any customized label
+    export MESOS_RESOURCES="ports(*):[1025-2180,2182-3887,3889-5049,5052-8079,8082-8180,8182-9999, 10001-32000]"
 fi
  
 mesos-slave
